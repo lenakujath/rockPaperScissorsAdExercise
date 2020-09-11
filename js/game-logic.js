5e1ab0e6360244c15c568e4909d40fb8ce3628eb
Lena Kujath <l.kujath@gmx.de>
2020-09-11 21:35:01 +0200

    wrote basic function to store player moves and values

---
+++ js/game-logic.js
@@ -0,0 +1,8 @@
+// All code should be written in this file.
+
+let playerOneMoveOneType, playerOneMoveTwoType, playerOneMoveThreeType;
+let playerTwoMoveOneType, playerTwoMoveTwoType, playTwoMoveThreeType;
+
+let playerOneMoveOneValue, playerOneMoveTwoValue, playerOneMoveThreeValue;
+let playerTwoMoveOneValue, playerTwoMoveTwoValue, playerTwoMoveThreeValue;
+
