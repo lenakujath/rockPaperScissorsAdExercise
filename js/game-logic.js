ff102b6290f6696d19e60a8324763ee08552b038
Lena Kujath <l.kujath@gmx.de>
2020-09-11 21:50:12 +0200

    added function for player moves

--- js/game-logic.js
+++ js/game-logic.js
@@ -6,3 +6,8 @@ let playerTwoMoveOneType, playerTwoMoveTwoType, playTwoMoveThreeType;
 let playerOneMoveOneValue, playerOneMoveTwoValue, playerOneMoveThreeValue;
 let playerTwoMoveOneValue, playerTwoMoveTwoValue, playerTwoMoveThreeValue;
 
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+
+    return;
+};
+
