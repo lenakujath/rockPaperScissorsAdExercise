f7b3035f29753d64d039ba28a8480f029e6ab0e2
Lena Kujath <l.kujath@gmx.de>
2020-09-12 17:15:13 +0200

    finished setter function for player moves and values

--- js/game-logic.js
+++ js/game-logic.js
@@ -8,6 +8,43 @@ let playerTwoMoveOneValue, playerTwoMoveTwoValue, playerTwoMoveThreeValue;
 
 const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
 
-    return;
+    if (!player || !moveOneType || !moveOneValue || !moveTwoType  ||  !moveTwoValue || !moveThreeType || !moveThreeValue){
+        return undefined;
+    } else if (moveOneType.includes('rock') === false && moveOneType.includes('paper') === false && moveOneType.includes('scissors') === false){
+        return undefined;
+    } else if (moveTwoType.includes('rock') === false && moveTwoType.includes('paper') === false && moveTwoType.includes('scissors') === false){
+        return undefined;
+    } else if (moveThreeType.includes('rock') === false && moveThreeType.includes('paper') === false && moveThreeType.includes('scissors') === false){
+        return undefined;
+    }else if (player.includes('Player One') === false && player.includes('Player Two') === false) {
+        return undefined;
+    } else  if (moveOneValue < 1 || moveTwoValue < 1 || moveThreeValue < 1 ){
+        return undefined;
+    } else if ((moveOneValue + moveTwoValue + moveThreeValue) > 99) {
+        return undefined;
+    } 
+
+    if (player === 'Player One'){
+        playerOneMoveOneType = moveOneType;
+        playerOneMoveTwoType = moveTwoType;
+        playerOneMoveThreeType = moveThreeType;
+        playerOneMoveOneValue = moveOneValue;
+        playerOneMoveTwoValue = moveTwoValue;
+        playerOneMoveThreeValue = moveThreeValue;
+     }
+    if (player === 'Player Two'){
+        playerTwoMoveOneType = moveOneType;
+        playerTwoMoveTwoType = moveTwoType;
+        playerTwoMoveThreeType = moveThreeType;
+        playerTwoMoveOneValue = moveOneValue;
+        playerTwoMoveTwoValue = moveTwoValue;
+        playerTwoMoveThreeValue = moveThreeValue;
+
+    };
 };
+
+setPlayerMoves('Player Two', 'rock', 10, 'paper', 2, 'scissors', 30);
+console.log(playerTwoMoveTwoType)
+
+
 
